fb8f7f80797849e64690ffc4bd40c142b2a519ee
Peter Schüller <ps@kr.tuwien.ac.at>
2011-09-09 10:09:55 +0000

    tiny bug

--- util/rm_ignored_unknown_svn_files.sh
+++ util/rm_ignored_unknown_svn_files.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 echo "going to remove these files:"
-svn status --no-ignore |grep "^[\?I]" |cut -b1-100
+svn status --no-ignore |grep "^[\?I]" |cut -b2-100
 echo "answer 'yes' to execute, abort with CTRL+C"
 read ANSWER
 echo "answer was '${ANSWER}'"
 if test "x${ANSWER}" = "xyes"; then
-  svn status --no-ignore |grep "^[\?I]" |cut -b1-100 |xargs rm -v
+  svn status --no-ignore |grep "^[\?I]" |cut -b2-100 |xargs rm -v
 fi
 
